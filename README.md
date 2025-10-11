31e90610857a701c696a2ef3ab1ce3838a8b6adb
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-10-11 15:46:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+prérequis
+
+CMake
+Ninja
+clang
+catch2
+
+command from root project
+cmake -S . -B build && cmake --build build && ./build/test_runner
---
+++ src/001.cpp
@@ -0,0 +1,28 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <type_traits>
+#include <iostream>
+
+//https://projecteuler.fr/p/1
+
+unsigned int P1( unsigned int number ) {
+    unsigned int result = 0;
+    for (int i = 1; i < number; i++) {
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i;
+        }
+    }
+    return result;
+}
+
+TEST_CASE( "Renvoie un int", "[P1]" ) {
+    REQUIRE(std::is_same<decltype(P1(10)), unsigned int>::value);
+}
+
+TEST_CASE( "tous les nombres naturels en dessous de 10 qui sont des multiples de 3 ou 5 donne la somme de 23", "[P1]" ) {
+        REQUIRE( P1(10) == 23 );
+}
+
+TEST_CASE( "tous les nombres naturels en dessous de 1000 qui sont des multiples de 3 ou 5 donne la somme de 1000", "[P1]" ) {
+        REQUIRE( P1(1000) == 233168 );
+}
