932cec692442c2d84d743a80e5a7885e30bfeba9
Marc Le Moigne <37547382+qQsss777@users.noreply.github.com>
2025-10-31 15:52:07 +0100

    p14

---
+++ src/014.cpp
@@ -0,0 +1,46 @@
+#include <catch2/catch_test_macros.hpp>
+#include <type_traits>
+#include <charconv>
+#include <iostream>
+
+// https://projecteuler.fr/p/14
+
+long Suite(long start)
+{
+    long currentCount = 0;
+    long result = 0;
+    int stop = 1;
+    for (int idx = start; idx > 0; --idx)
+    {
+        long current = idx;
+        int count = 0;
+        while (current > stop)
+        {
+            count++;
+            if (current % 2 == 0)
+            {
+                current = current / 2;
+            }
+            else
+            {
+                current = current * 3 + 1;
+            }
+        }
+        if (count > currentCount)
+        {
+            currentCount = count;
+            result = idx;
+        }
+    }
+    return result;
+}
+
+TEST_CASE("Renvoie un long", "[Suite]")
+{
+    REQUIRE(std::is_same<decltype(Suite(999999)), long>::value);
+}
+
+TEST_CASE("Pour 999999 renvoie 9", "[Suite]")
+{
+    REQUIRE(Suite(999999) == 837799);
+}
